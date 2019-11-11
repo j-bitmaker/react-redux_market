1a3ad152bad91057d362a70087715c635fadb26c
j-bitmaker <nilolyaegorytch@yandex.ru>
2019-11-11 13:07:20 +0300

    remove modules

--- src/containers/counter/index.js
+++ src/containers/counter/index.js
@@ -18,24 +18,6 @@ const Counter = ({count, inc, dec}) =>{
                 <i className="fa fa-plus-circle"/>
             </button>
            </div>
-            {/* <div className='col-6'>
-            <button
-            onClick={dec}
-            className="btn btn-outline-primary btn-sm">
-            <i className="fa fa-minus-circle minus" />
-            </button>
-            </div>
-            <div className='col-6'>
-            <h5>  {count}</h5>
-            </div>
-            <div className='col-6'>
-                h
-           
-            </div> <button
-            onClick={inc}
-            className="btn btn-outline-warning btn-sm">
-                <i className="fa fa-plus-circle"/>
-            </button>         */}
         </div>
     
     )
